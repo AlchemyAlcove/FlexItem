33b907505d19a00e4889b6a1d0fb85eec91e00bb
Mike Hoffert <mike.hoffert@gmail.com>
2018-07-18 08:06:52 -0600

    Dist files

--- dist/flexItem/flex.style.js
+++ dist/flexItem/flex.style.js
@@ -6,12 +6,12 @@ Object.defineProperty(exports, "__esModule", {
 
 var _templateObject = _taggedTemplateLiteral(["\n  &.flex-item {\n    position: relative;\n    display: flex;\n    flex-direction: column;\n    margin: 0;\n    max-width: 100%;\n  }\n"], ["\n  &.flex-item {\n    position: relative;\n    display: flex;\n    flex-direction: column;\n    margin: 0;\n    max-width: 100%;\n  }\n"]);
 
-var _styledComponents = require("styled-components");
+var _reactEmotion = require("react-emotion");
 
-var _styledComponents2 = _interopRequireDefault(_styledComponents);
+var _reactEmotion2 = _interopRequireDefault(_reactEmotion);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _taggedTemplateLiteral(strings, raw) { return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
 
-exports.default = _styledComponents2.default.div(_templateObject);
+exports.default = (0, _reactEmotion2.default)("div")(_templateObject);
